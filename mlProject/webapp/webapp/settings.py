cf6477280f50427e1b55d44740b9ac628bc1cbff
Aditya Narayan Maurya <90122182+Aditya9329@users.noreply.github.com>
2023-03-21 09:28:37 -0700

    Update settings.py

--- mlProject/webapp/webapp/settings.py
+++ mlProject/webapp/webapp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-&9hor4h67-zo2!++c1!%=ag9nz+z6whm@$3^v-53o)u4=e*m&l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
